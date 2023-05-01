57d89752e1b6a3e0b90d00de131fdfb5c2fd97a4
Dekir <Dekir@DESKTOP>
2023-05-01 20:25:46 +0300

    Added Integrals

---
+++ CalculatingDefiniteIntegrals/Integrals.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatingDefiniteIntegrals
+{
+    internal static class Integrals
+    {
+        public static double Rectangle(Func<double, double> func, double left, double right, double h = 0.0001)
+        {
+            if (right - left < h)
+            {
+                return 0;
+            }
+
+            double sum = 0d;
+
+            for (int i = 0; i < (int)((right - left) / h); i++)
+            {
+                sum += func(left + i * h + h / 2);
+            }
+
+            return sum * h;
+        }
+
+        public static double Trapezoid(Func<double, double> func, double left, double right, double h = 0.0001)
+        {
+            if (right - left < h)
+            {
+                return 0;
+            }
+
+            double sum = 0d;
+
+            for (int i = 1; i < (int)((right - left) / h) - 1; i++)
+            {
+                sum += func(left + i * h);
+            }
+
+            return (sum + 0.5 * (func(left) + func(right))) * h;
+        }
+
+        public static double Simpson(Func<double, double> func, double left, double right, double h = 0.0001)
+        {
+            if (right - left < h)
+            {
+                return 0;
+            }
+
+            double sum = 0d;
+
+            for (int i = 0; i < (int)((right - left) / h); i++)
+            {
+                sum += func(left + h * i) * (i.IsOdd() ? 2 : 4);
+            }
+
+            return sum * h / 3;
+        }
+
+        private static bool IsOdd(this int i) => (i & 1) == 0;
+    }
+}
---
+++ CalculatingDefiniteIntegrals/Program.cs
@@ -0,0 +1,9 @@
+﻿using CalculatingDefiniteIntegrals;
+
+Func<double, double> f = x => 1 / Math.Sqrt(x*x*x + 1);
+
+Console.WriteLine(Integrals.Rectangle(f, 0, 1));
+Console.WriteLine(Integrals.Trapezoid(f, 0, 1));
+Console.WriteLine(Integrals.Simpson(f, 0, 1));
+
+Console.ReadKey();
