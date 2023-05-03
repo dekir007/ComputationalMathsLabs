d807b0401fc38580370a26791117fc41d8031f0f
Dekir <Dekir@DESKTOP>
2023-05-03 20:32:31 +0300

    Added MonteCarlo integration and PI

---
+++ Monte Carlo/MonteCarlo.cs
@@ -0,0 +1,32 @@
+﻿namespace Monte_Carlo
+{
+    internal static class MonteCarlo
+    {
+        public static double Integral(Func<double, double> func, double a, double b, int n = 750_000)
+        {
+            var sum = 0d;
+            var rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                sum += func((b - a) * rnd.NextDouble() + a);
+            }
+            return (sum / n * (b - a));
+        }
+
+        public static double PI(int n = 750_000)
+        {
+            double circle = 0;
+            var rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                double x = rnd.NextDouble();
+                double y = rnd.NextDouble();
+                if (x*x+y*y <=1)
+                {
+                    circle++;
+                }
+            }
+            return 4 * circle / n;
+        }
+    }
+}
