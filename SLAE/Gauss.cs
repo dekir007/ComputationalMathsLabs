83994c93df7a14509dd45875469a1ab2598c5a36
Dekir <Dekir@DESKTOP>
2023-05-17 00:29:26 +0300

    Added Matrix class and Gauss method for single solution

---
+++ SLAE/Gauss.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLAE
+{
+    internal class Gauss
+    {
+        private Matrix SystemMatrix;
+        private Matrix Extended;
+
+        public Gauss(Matrix system, List<double> free)
+        {
+            SystemMatrix = system;
+            Extended = system.GetExtended(free);
+        }
+
+        public SolutionsCount GetCountOfSolutions()
+        {
+            if (!IsSolvable())
+            {
+                return SolutionsCount.None;
+            }
+
+            if (SystemMatrix.Rank == SystemMatrix.ColumnsCount)
+            {
+                return SolutionsCount.Single;
+            }
+
+            return SolutionsCount.Infinity;
+        }
+
+        public bool IsSolvable()
+        {
+            return SystemMatrix.Rank == Extended.Rank;
+        }
+
+        public (SolutionsCount count, List<double> results) Solve()
+        {
+            List<double> results = new List<double>();
+            var solutionsCount = GetCountOfSolutions();
+            switch (solutionsCount)
+            {
+                case SolutionsCount.None:
+                    break;
+                case SolutionsCount.Single:
+                    results = SolveSingle();
+                    break;
+                case SolutionsCount.Infinity:
+                    results = SolveInfinite();
+                    break;
+                default:
+                    throw new Exception("There are only three cases");
+            }
+            return (solutionsCount, results);
+        }
+
+        private List<double> SolveSingle()
+        {
+            Extended = Extended.ToUpperTriangle();
+            Extended = Extended.ToLowerTriangle();
+
+            for (int i = 0; i < Extended.RowsCount; i++)
+            {
+                Extended[i][^1] /= Extended[i][i];
+                Extended[i][i] = 1d;
+            }
+
+            return Extended.nums.Select(x => x.Last()).ToList();
+        }
+
+        private List<double> SolveInfinite()
+        {
+            return new List<double>();
+        }
+    }
+}
--- SLAE/Program.cs
+++ SLAE/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using SLAE;
+
+var m1 = new Matrix(new List<List<double>>
+        {
+            new List<double> { 5, -1, 3 },
+            new List<double> { 1, -4, 2 },
+            new List<double> { 2, -1, 5 },
+        });
+var m2 = new Matrix(new List<List<double>>
+        {
+            new List<double> { 1, -1, 2 },
+            new List<double> { 2, -2, 4 },
+            new List<double> { -1, 1, -2 },
+        });
+var m3 = new Matrix(new List<List<double>>
+        {
+            new List<double> { 1, 2, 3 },
+            new List<double> { 1, -1, 1 },
+            new List<double> { 1, 3, -1 },
+        });
+//Console.WriteLine(m3.Rank);
+var freeVector = new List<double>() { 2, 0, -2 };
+Console.WriteLine(string.Join(" " , new Gauss(m3, freeVector).Solve().results));
